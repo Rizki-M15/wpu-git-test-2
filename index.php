e49ca735feb2f98f2308d039e7c24acd11680592
kiki15 <rizki.hidayat@next-ti.co.id>
2022-08-05 14:14:25 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello..</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque vitae consequuntur aliquam adipisci, cupiditate doloremque sit debitis veniam eum, laborum quibusdam ad culpa repudiandae voluptate tempore. Culpa velit laboriosam eligendi?</p>
+    <ul>
+        <li>buku</li>
+        <li>tas</li>
+    </ul>
 </body>
 </html>
